1002c53e58bb6e517b788e6a6fcfdab705583163
ToxichUA <ventel911@yandex.ru>
2017-12-07 20:04:29 +0200

    Demonstration version

---
+++ Assistant/wwwroot/js/cart.js
@@ -0,0 +1,15 @@
+﻿(function ($, document) {
+    "use strict";
+    var $document = $(document);
+
+    function submitForm(e) {
+        e.preventDefault();
+
+        var $form = $(e.currentTarget),
+            $q = $form.find('[data-quantity]');
+        console.log($q.val());
+    }
+
+    $document.on('submit', 'form[data-form]', submitForm);
+
+})(jQuery, document);
